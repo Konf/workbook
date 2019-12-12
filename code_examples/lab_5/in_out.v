41368bc21183c8f3956374638358a2cda2e863c2
Evgeniy Primakov <conf@confusion.su>
2019-12-12 12:57:05 +0300

    Удалена куча неиспользуемых листингов

--- code_examples/lab_5/in_out.v
+++
@@ -1,6 +0,0 @@
-module ps2_keyboard(  
-  input             areset,
-  input             ps2_clk,
-  input             ps2_dat,
-  output  reg       valid_data,
-  output     [7:0]  data);
--- code_examples/lab_6/flash_status.v
+++
@@ -1,3 +0,0 @@
-wire   timeout   = data_from_flash[4];
-assign status_ok = ( r_data == w_data )
-assign error     = ~status_ok && timeout;
