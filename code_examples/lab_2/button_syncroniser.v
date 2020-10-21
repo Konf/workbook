f80f16e0b1ce46d4e4cb2299191fabaedee7fdeb
murenkov <yaroslav.murenkov@gmail.com>
2020-10-21 23:07:39 +0300

    Исправлено использование неблокирующего присваивания вне блока always

--- code_examples/lab_2/button_syncroniser.v
+++ code_examples/lab_2/button_syncroniser.v
@@ -7,5 +7,5 @@ always @(posedge clk) begin
   button_syncroniser[2] <= button_syncroniser[1];
 end
 
-assign button_was_pressed <= ~button_syncroniser[2]
+assign button_was_pressed = ~button_syncroniser[2]
                             & button_syncroniser[1];
